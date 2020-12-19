f45cf701bc9e0b2354ea6eb1986494d145114db5
i_am_akash_ <ay157311@gmail.com>
2020-12-19 20:26:32 +0530

    here we access context with the help of consumer

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -2,28 +2,26 @@ import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
-  // this can be done only in class component
-  // it specify that which context we want to use so here we are using ThemeContext
-  // the name of context must be contextType
-  // How it works =>
-  // first it find provider of context i.e. ThemeContext
-  // and it gets the data provided by the provider
-  // and it takes the data and attaches to context property of this component
-  static contextType = ThemeContext;
   render() { 
-    // and it takes the data and attaches to context property of current component
-    console.log(this.context);
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return ( 
-      <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
-        <ul>
-          <li style={{ background: theme.ui }}>the way of kings</li>
-          <li style={{ background: theme.ui }}>the name of the wind</li>
-          <li style={{ background: theme.ui }}>the final empire</li>
-        </ul>
-      </div>
-    );
+    // we can also access the context with the help of consumer
+    // This approach can also be use in functional component
+    // the benefit of this approach is we can consume multiple context in one component
+    // so we use <ThemeContext.Consumer>
+    // and inside that we provide function which take context as argument.
+    // and also we return tha jsx in that function
+    <ThemeContext.Consumer> { (context)=>{
+      const { isLightTheme, light, dark } = this.context;
+      const theme = isLightTheme ? light : dark;
+      return ( 
+        <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+          <ul>
+            <li style={{ background: theme.ui }}>the way of kings</li>
+            <li style={{ background: theme.ui }}>the name of the wind</li>
+            <li style={{ background: theme.ui }}>the final empire</li>
+          </ul>
+        </div>
+      );
+    }}</ThemeContext.Consumer>
   }
 }
  
