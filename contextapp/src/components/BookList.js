7ee0c3fed44a62b7e9631d6fc23943f3af6a6e03
i_am_akash_ <ay157311@gmail.com>
2020-12-19 20:30:50 +0530

    in previous commit getting some error

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -9,19 +9,21 @@ class BookList extends Component {
     // so we use <ThemeContext.Consumer>
     // and inside that we provide function which take context as argument.
     // and also we return tha jsx in that function
-    <ThemeContext.Consumer> { (context)=>{
-      const { isLightTheme, light, dark } = this.context;
-      const theme = isLightTheme ? light : dark;
-      return ( 
-        <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
-          <ul>
-            <li style={{ background: theme.ui }}>the way of kings</li>
-            <li style={{ background: theme.ui }}>the name of the wind</li>
-            <li style={{ background: theme.ui }}>the final empire</li>
-          </ul>
-        </div>
-      );
-    }}</ThemeContext.Consumer>
+    return(
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+        return ( 
+          <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+            <ul>
+              <li style={{ background: theme.ui }}>the way of kings</li>
+              <li style={{ background: theme.ui }}>the name of the wind</li>
+              <li style={{ background: theme.ui }}>the final empire</li>
+            </ul>
+          </div>
+        );
+      }}</ThemeContext.Consumer>
+    );    
   }
 }
  
