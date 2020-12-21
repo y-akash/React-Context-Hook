f096ec60ac518d3336a9679e7b238a48efc2c16a
i_am_akash_ <ay157311@gmail.com>
2020-12-21 15:46:50 +0530

    accessing multiple context using hook

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,33 +1,25 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { AuthContext } from '../contexts/AuthContext';
 
-class Navbar extends Component {
-  render() { 
-    return ( 
-      // here we are consuming 2 context by nesting to other
-      <AuthContext>{(authContext) => {return (
-        <ThemeContext.Consumer>{(themeContext) => {
-          const { isAuthenticated, toggleAuth } = authContext;
-          const { isLightTheme, light, dark } = themeContext;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <nav style={{ background: theme.ui, color: theme.syntax }}>
-              <h1>Context App</h1>
-              <div onClick={() => toggleAuth()}>
-                { isAuthenticated ? 'Logged in' : 'Logged out' }
-              </div>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </nav>
-          )
-        }}</ThemeContext.Consumer>
-      )}}</AuthContext>
-    );
-  }
+const Navbar = () => {
+  // here we are using multiple context using useContext hook
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <nav style={{ background: theme.ui, color: theme.syntax }}>
+      <h1>Context App</h1>
+      <div onClick={() => toggleAuth()}>
+        { isAuthenticated ? 'Logged in' : 'Logged out' }
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
 }
  
 export default Navbar;
